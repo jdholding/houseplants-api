63bb90c15a52674289308ab6dc2b0f58dc3d2857
JD Holding <holding.jd@gmail.com>
2023-01-16 21:32:57 +0100

    add repository and mybatis config

--- application/src/main/java/rc/holding/houseplants/HouseplantsApplication.java
+++ application/src/main/java/rc/holding/houseplants/HouseplantsApplication.java
@@ -3,7 +3,8 @@ package rc.holding.houseplants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = {"rc.holding.*"})
 public class HouseplantsApplication {
 
 	public static void main(String[] args) {
--- repository-api/src/main/java/rc/holding/houseplants/repository/api/PlantRepository.java
+++ repository-api/src/main/java/rc/holding/houseplants/repository/api/PlantRepository.java
@@ -1,5 +1,11 @@
-package rc.houseplants.repository.api;
+package rc.holding.houseplants.repository.api;
+
+import java.util.List;
+
+
+import rc.holding.houseplants.domain.Plant;
+
 
 public interface PlantRepository {
-    List<Plant> getAllPlants();
+    List<Plant> findAllPlants();
 }
